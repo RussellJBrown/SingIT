6ca5a3d0a344949c06f6aed3eea23499ad7b1b86
RussellJBrown <33341699+RussellJBrown@users.noreply.github.com>
2019-11-10 13:29:53 -0800

    Update librosaVocalExtract.py

--- librosaVocalExtract.py
+++ librosaVocalExtract.py
@@ -2,15 +2,56 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
-
 import librosa.display
 
-#This program attempts to get
-#librosa to work, may need to remove later
-y, sr = librosa.load('99German.wav')
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+def to_str(var):
+    return str(list(np.reshape(np.asarray(var), (1, np.size(var)))[0]))[1:-1]
 
-print(beat_frames)
-print(tempo)
-print(beat_times)
+
+def libroExtract(inputSong):
+    y, sr = librosa.load(inputSong)
+    S_full, phase = librosa.magphase(librosa.stft(y))
+
+    S_filter = librosa.decompose.nn_filter(S_full,
+                                       aggregate=np.median,
+                                       metric='cosine',
+                                       width=int(librosa.time_to_frames(2, sr=sr)))
+
+    S_filter = np.minimum(S_full, S_filter)
+    marginI, marginV = 2, 10
+    power = 2
+    mask_i = librosa.util.softmask(S_filter,
+                               marginI * (S_full - S_filter),
+                               power=power)
+
+    mask_v = librosa.util.softmask(S_full - S_filter,
+                               marginV * S_filter,
+                               power=power)
+
+
+    S_foreground = mask_v * S_full
+    S_background = mask_i * S_full
+
+    centralCentroid = librosa.feature.spectral_centroid(y=y,sr=sr)
+    normalMean = np.mean(centralCentroid)
+    normalSTD  = np.std(centralCentroid)
+
+    #TODO: change to a string variable and add if statements for
+    #other languages
+    f = open('NormalScatterEnglish.txt','a')
+    f.write(to_str(normalMean))
+    f.write(" ")
+    f.write(to_str(normalSTD))
+    f.write("\n")
+    f.close()
+
+    #Currently Checking here for errors
+
+    vocalMean = np.mean(S_foreground)
+    vocalSTD = np.std(S_foreground)
+    f = open('VocalExtractedScatterEnglish.txt','a')
+    f.write(to_str(vocalMean))
+    f.write(" ")
+    f.write(to_str(vocalSTD))
+    f.write("\n")
+    f.close()
